88277a072bf450e22c05463bceafed1a62bd1d60
anthony burt <anthonyburt@gmail.com>
2018-02-24 22:04:41 -0500

    refactor to more components, removed unnecessary code, fixed data for stacked graph

---
+++ src/components/GraphPassFail.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory'
+
+class GraphPassFail extends React.Component {
+	render () {
+
+        const passes = [
+          {testrun: 1, results: 20},
+          {testrun: 2, results: 4},
+          {testrun: 3, results: 20},
+          {testrun: 4, results: 50}
+        ];
+
+        const fails = [
+          {testrun: 1, results: 10},
+          {testrun: 2, results: 9},
+          {testrun: 3, results: 0},
+          {testrun: 4, results: 20}
+        ];
+
+        return (
+
+            <VictoryChart
+                domainPadding={20}
+                theme={VictoryTheme.material}
+            >
+                <VictoryAxis
+                    // tickValues specifies both the number of ticks and where
+                    // they are placed on the axis
+                    tickValues={[1, 2, 3, 4]}
+                    tickFormat={["Shop X", "Checkout", "OA", "Order Mgmt"]}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    // tickFormat specifies how ticks should be displayed
+                    tickFormat={(x) => (`${x}`)}
+                />
+                <VictoryStack>
+                    <VictoryBar
+                        data={fails}
+                        x="testrun"
+                        y="results"
+                    />
+                    <VictoryBar
+                            data={passes}
+                            x="testrun"
+                            y="results"
+                        />
+                </VictoryStack>
+            </VictoryChart>
+        )
+	}
+}
+
+export default GraphPassFail
--- src/components/TestResults.js
+++ src/components/TestResults.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Test from '../components/Test'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
-import Moment from 'react-moment'
-import 'moment-timezone'
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory'
-import Gridz from '../components/Gridz'
+import ReactGridLayout from 'react-grid-layout'
+
+import GraphPassFail from '../components/GraphPassFail'
+import TableResults from '../components/TableResults'
 
 
 class TestResults extends React.Component {
@@ -17,130 +13,25 @@ class TestResults extends React.Component {
 			return (<div className='w-100 flex justify-center'>Loading...</div>)
         }
 
-        const data2012 = [
-          {quarter: 1, earnings: 13000},
-          {quarter: 2, earnings: 16500},
-          {quarter: 3, earnings: 14250},
-          {quarter: 4, earnings: 19000}
-        ];
-
-        const data2013 = [
-          {quarter: 1, earnings: 15000},
-          {quarter: 2, earnings: 12500},
-          {quarter: 3, earnings: 19500},
-          {quarter: 4, earnings: 13000}
-        ];
-
-        const data2014 = [
-          {quarter: 1, earnings: 11500},
-          {quarter: 2, earnings: 13250},
-          {quarter: 3, earnings: 20000},
-          {quarter: 4, earnings: 15500}
-        ];
-
-        const data2015 = [
-          {quarter: 1, earnings: 18000},
-          {quarter: 2, earnings: 13250},
-          {quarter: 3, earnings: 15000},
-          {quarter: 4, earnings: 12000}
+        const layout = [
+          {i: 'a', x: 0, y: 0, w: 3, h: 10},
+          {i: 'b', x: 1, y: 0, w: 10, h: 5},
+          {i: 'c', x: 4, y: 0, w: 1, h: 2}
         ];
 
 		return (
-		    <div className='w-100 flex justify-center'>
-		        <Gridz></Gridz>
-                <div className=''>
-                    <VictoryChart
-                        theme={VictoryTheme.material}
-                        domainPadding={20}
-                    >
-                        <VictoryAxis
-                          // tickValues specifies both the number of ticks and where
-                          // they are placed on the axis
-                          tickValues={[1, 2, 3, 4]}
-                          tickFormat={["Shop X", "Checkout", "OA", "Order Mgmt"]}
-                        />
-                        <VictoryAxis
-                          dependentAxis
-                          // tickFormat specifies how ticks should be displayed
-                          tickFormat={(x) => (`${x / 1000}`)}
-                        />
-                        <VictoryStack>
-                            <VictoryBar
-                                data={data2012}
-                                x="quarter"
-                                y="earnings"
-                            />
-                            <VictoryBar
-                                data={data2013}
-                                x="quarter"
-                                y="earnings"
-                            />
-                            <VictoryBar
-                                data={data2014}
-                                x="quarter"
-                                y="earnings"
-                            />
-                            <VictoryBar
-                                data={data2015}
-                                x="quarter"
-                                y="earnings"
-                            />
-                        </VictoryStack>
-                    </VictoryChart>
+		    <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+		        <div key="a">
+                    <GraphPassFail/>
                 </div>
-				<div className=''>
-					<ReactTable
-						data={this.props.data.allTestResultses}
-						columns={columns}
-						defaultSorted={[
-                            {
-                                id: "createdAt",
-                                desc: true
-                            }
-                        ]}
-                        defaultPageSize={10}
-                            style={{
-                            height: "400px" // This will force the table body to overflow and scroll, since there is not enough room
-                        }}
-                        className="-striped -highlight"
-					/>
+				<div key="b">
+				    {/*<TableResults/>*/}
 				</div>
-
-			</div>
+			<div key="c">grid 3</div>
+          </ReactGridLayout>
         )
     }
 }
-
-const columns = [{
-        Header: 'Date',
-        accessor: 'createdAt',
-        Cell: props => <Moment format="MMMM DD, YYYY @ hh:mm a" date={props.value} />,
-        width: 300,
-    }, {
-        Header: 'Build #',
-        accessor: 'buildNumber',
-        width: 100
-    }, {
-  	    Header: 'Product',
-  	    accessor: 'products.name',
-  	    width: 100,
-    }, {
-  	    Header: 'OS',
-  	    accessor: 'os',
-  	    width: 100,
-    }, {
-        Header: 'Browser',
-        accessor: 'browser',
-        width: 100,
-    }, {
-        Header: 'Test',
-        accessor: 'testname',
-        width: 100,
-    }, {
-        Header: 'Status',
-        accessor: 'testStatus',
-        maxwidth: 100
-  }]
 
 const TestListQuery = gql`
 	query allTestResultses {
