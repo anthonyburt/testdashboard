cd4d0a353763fe4df64150b5974fbda4f032de2c
anthony burt <anthonyburt@gmail.com>
2018-03-07 22:51:07 -0500

    f

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
     constructor(props) {
@@ -14,30 +14,30 @@ class Header extends Component {
 
 
         return (
-            <nav>
+
                 <Menu color='blue' inverted size='tiny' icon='labeled'>
-                    <Menu.Item as={ Link } name='home' active={this.props.activeItem === 'home'} onClick={this.handleItemClick} to='/'>
+                    <Menu.Item as={ NavLink } name='home' active={this.props.activeItem === 'home'} onClick={this.handleItemClick} to='/'>
                         <Icon name='home' />
                         Home
                     </Menu.Item>
-                    <Menu.Item as={ Link } activeClassName="active" name='shopx'  active={this.props.activeItem === 'shopx'} onClick={this.handleItemClick} to='shopx'>
+                    <Menu.Item as={ NavLink } activeClassName="active" name='shopx'  active={this.props.activeItem === 'shopx'} onClick={this.handleItemClick} to='shopx'>
                         <Icon name='shopping bag'/>
                         Shop X
                     </Menu.Item>
-                    <Menu.Item as={ Link } name='checkout' active={this.props.activeItem === 'checkout'} onClick={this.handleItemClick} to='checkout'>
+                    <Menu.Item as={ NavLink } name='checkout' active={this.props.activeItem === 'checkout'} onClick={this.handleItemClick} to='checkout'>
                         <Icon name='credit card alternative'/>
                         Checkout
                     </Menu.Item>
-                    <Menu.Item as={ Link } name='orderassist' active={this.props.activeItem === 'orderassist'} onClick={this.handleItemClick} to='/orderassist'>
+                    <Menu.Item as={ NavLink } name='orderassist' active={this.props.activeItem === 'orderassist'} onClick={this.handleItemClick} to='/orderassist'>
                         <Icon name='doctor'/>
                         Order Assist
                     </Menu.Item>
-                    <Menu.Item as= { Link} name='ordermanagement'  active={this.props.activeItem === 'ordermanagement'} onClick={this.handleItemClick} to='/ordermanagement'>
+                    <Menu.Item as= { NavLink } name='ordermanagement'  active={this.props.activeItem === 'ordermanagement'} onClick={this.handleItemClick} to='/ordermanagement'>
                         <Icon name='database'/>
                         Order Mgmt.
                     </Menu.Item>
                 </Menu>
-            </nav>
+
         )
     }
 }
