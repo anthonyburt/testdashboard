766258fb2f6346fa92b2642c7584590cc33c1aad
Anthony Burt <Anthony.Burt@aarons.com>
2018-03-10 17:47:51 -0500

    stats api call

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const baseAPI = '/api';
+
+const testService = {
+
+  get() {
+    return new Promise((resolve, reject) => {
+        console.log("get stats");
+        fetch(`${baseAPI}/stats`, {
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(err => {
+        reject(err);
+        });
+    });
+  }
+};
+
+export default testServiceS;
