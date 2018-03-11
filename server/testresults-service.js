cd68210e23930719c2dd0d6519982e849f3de220
anthony burt <anthonyburt@gmail.com>
2018-03-10 19:19:36 -0500

    commenting out broken query:

--- server/testresults-service.js
+++ server/testresults-service.js
@@ -5,7 +5,7 @@ require('./mongo').connect();
 
 function get(req, res) {
 
-    const queryIWant = TestResults.aggregate([{ $group: { _id: "null", "total_duration": { $sum: "$duration" }, "total_tests": { $sum: 1 }, "total_passes": { $sum: { $cond : { if: { $eq: ["$status", true]}, then: 1, else: 0 } } }, "total_fails": { $sum: { $cond : { if: { $eq: ["$status", false]}, then: 1, else: 0 } } } } } ]).pretty();
+    //const queryIWant = TestResults.aggregate([{ $group: { _id: "null", "total_duration": { $sum: "$duration" }, "total_tests": { $sum: 1 }, "total_passes": { $sum: { $cond : { if: { $eq: ["$status", true]}, then: 1, else: 0 } } }, "total_fails": { $sum: { $cond : { if: { $eq: ["$status", false]}, then: 1, else: 0 } } } } } ]).pretty();
 
     const docquery = TestResults.aggregate([ {$group: { _id: null, count: { $sum: 1} } } ]);
     docquery
